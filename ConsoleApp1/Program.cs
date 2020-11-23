10df64077f2a24df955dde6fdcfdedc4fee5afc2
Kailon <kailon.guerra@infinitsolucoes.com.br>
2020-11-23 10:45:31 -0300

    Acrescentada uma mensagem de bom dia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
         {
 
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Bom dia");
+            
             Console.ReadLine();
 
         }
