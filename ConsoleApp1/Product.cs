34395ef502cfffd330c80b92491b739bf640ddab
Kailon <kailon.guerra@infinitsolucoes.com.br>
2020-11-23 10:42:57 -0300

    Projeto Iniciado, mostrando a mensagem Ola mundo

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+
+        }
+    }
+}
+
